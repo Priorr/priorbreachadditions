bad8f38f1e5022b5ff48316bc78eafeaa5f1f3ae
Priorr <priorrrrr@gmail.com>
2017-07-11 01:41:39 +1000

    Update sv_dropammo.lua

--- priorbreachadditions/lua/autorun/server/sv_dropammo.lua
+++ priorbreachadditions/lua/autorun/server/sv_dropammo.lua
@@ -2,24 +2,24 @@ if (SERVER) then
 hook.Add( "PlayerDeath", "DropAmmo", function(ply)
 whererthey = ply:GetPos()	
 	arammo = ply:GetAmmoCount( "AR2" )
-				if arammo >= 1 then 
+		if arammo >= 1 then 
 			local assaultammo = ents.Create("item_rifleammo") 
-				assaultammo:SetPos( whererthey )
-				assaultammo:Spawn()
-			end
+			assaultammo:SetPos( whererthey )
+			assaultammo:Spawn()
+		end
 
 	smgammo = ply:GetAmmoCount( "SMG1" )
-			if smgammo >= 1 then 
-				local submachineammo = ents.Create("item_smgammo") 
-				submachineammo:SetPos( whererthey )
-				submachineammo:Spawn()
-			end	
+		if smgammo >= 1 then 
+			local submachineammo = ents.Create("item_smgammo") 
+			submachineammo:SetPos( whererthey )
+			submachineammo:Spawn()
+		end	
 
 	pisammo = ply:GetAmmoCount( "Pistol" )
 		if pisammo >= 1 then 
 			local pistolammo = ents.Create("item_pistolammo") 
-				pistolammo:SetPos( whererthey )
-				pistolammo:Spawn()
+			pistolammo:SetPos( whererthey )
+			pistolammo:Spawn()
 		end		
 end)
 end
